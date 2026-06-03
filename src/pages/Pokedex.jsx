36b6e95cc6e3f39980e6eaadef390d43e145f836
Mateus Cruz <110851026+Mateuscruz19@users.noreply.github.com>
2026-06-03 19:39:14 -0300

    Service updated

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,22 +1,14 @@
 import { useEffect, useState } from 'react'
 import Card from '../components/Card'
-import { pokemonIds } from '../data/pokemons'
-import { getCustomPokemons } from '../services/customPokemons'
+import { getAllPokemons } from '../services/pokeAPI'
 import './Pokedex.css'
 
 export default function Pokedex() {
   const [pokemons, setPokemons] = useState([])
 
   useEffect(() => {
-    // pega os pokémons que a gente cadastrou (do localStorage)
-    const meus = getCustomPokemons()
-
-    // busca os 151 da API e junta tudo (os nossos primeiro)
-    Promise.all(
-      pokemonIds.map((id) =>
-        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((r) => r.json())
-      )
-    ).then((daApi) => setPokemons([...meus, ...daApi]))
+    // o service busca da API e já junta com os custom do localStorage
+    getAllPokemons().then((lista) => setPokemons(lista))
   }, [])
 
   return (
---
+++ src/services/pokeAPI.js
@@ -0,0 +1,17 @@
+import { pokemonIds } from '../data/pokemons'
+import { getCustomPokemons } from './customPokemons'
+
+// PRIVADO: só busca da API REST (não exporta)
+function fetchAPIPokemon() {
+  return Promise.all(
+    pokemonIds.map((id) =>
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((r) => r.json())
+    )
+  )
+}
+
+// PÚBLICO: junta os custom (localStorage) com os da API
+export function getAllPokemons() {
+  const meus = getCustomPokemons()
+  return fetchAPIPokemon().then((daApi) => [...meus, ...daApi])
+}
